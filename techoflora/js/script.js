f8e578f40600cf96d0cedc997459b3c5349869b6
1huali <wanhuali2000@gmail.com>
2024-02-06 00:07:06 -0500

    controller work for first page (figure out for other pages tbd)

--- techoflora/js/script.js
+++ techoflora/js/script.js
@@ -5,9 +5,14 @@ Full screen code: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_f
 
 "use strict";
 window.onload = (event) => {
+//img array  ❀ ❀❀✿❀ ❀ ❀❁❀❀❀  ❀❀✿❀❀❀❀  ❀ ❀❀✿❀ ❀ ❀❁❀❀❀  ❀❀✿❀❀❀❀ 
 
+let ying1hua1ImgArray = [`assets/images/flora_half1-bendo_naturel.00_07_43_16.Still024.jpg`,`assets/images/flora_half1-bendo_naturel.00_07_47_19.Still008 copie.jpg`]
+
+
+//top nav menu  ❀ ❀❀✿❀ ❀ ❀❁❀❀❀  ❀❀✿❀❀❀❀  ❀ ❀❀✿❀ ❀ ❀❁❀❀❀  ❀❀✿❀❀❀❀ 
     document.getElementById("springBtn").addEventListener("click", function(){
-        window.location.href="spring.html";
+        window.location.href="ying1hua1.html";
     });
     document.getElementById("winterBtn").addEventListener("click", function(){
         window.location.href="winter.html";
@@ -22,18 +27,38 @@ window.onload = (event) => {
         window.location.href="huangsehua1.html";
     });
 
-    // CONTROLLER PLAYER BUTTON: 
+    // CONTROLLER PLAYER BUTTON  ❀ ❀❀✿❀ ❀ ❀❁❀❀❀  ❀❀✿❀❀❀❀  ❀ ❀❀✿❀ ❀ ❀❁❀❀❀  ❀❀✿❀❀❀❀ 
 let rightButton= document.querySelector(".rightarrow");
 let leftButton= document.querySelector(".leftarrow");
+let currentImg=0;
 
-rightButton.addEventListener("click", function(){
-    console.log("hello");
-});
 leftButton.addEventListener("click", function(){
-    console.log("hello");
-});
+    currentImg++;
+    if (currentImg > ying1hua1ImgArray.length-1){
+        currentImg = 0;
+    }
+    console.log(currentImg);
+    changeImg(ying1hua1ImgArray[currentImg]);
 
-//full screen option button : 
+});
+rightButton.addEventListener("click", function(){
+    currentImg--;
+    if (currentImg<=-1){
+        currentImg=ying1hua1ImgArray.length-1;
+    }
+    console.log(currentImg);
+    changeImg(ying1hua1ImgArray[currentImg]);
+});
+//?? do I have to code for each page or there is a generic way to go about the code
+
+
+function changeImg(newLink){
+    let img = document.querySelector('.myimage'); 
+    // Change the href attribute
+    img.src = newLink;
+}
+
+//full screen option button  ❀ ❀❀✿❀ ❀ ❀❁❀❀❀  ❀❀✿❀❀❀❀  ❀ ❀❀✿❀ ❀ ❀❁❀❀❀  ❀❀✿❀❀❀❀  
 let elem= document.querySelector(".myimage");
 
 let fullscreenButton = document.querySelector(".fullscreen-button");
