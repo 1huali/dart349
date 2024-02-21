0eece0976a0256a13f05c6aeabbab97e6bbf7bde
1huali <wanhuali2000@gmail.com>
2024-02-21 00:12:37 -0500

    grid & gr works ; need to create individual pages

--- assignment 5/js/script.js
+++ assignment 5/js/script.js
@@ -8,4 +8,33 @@ author, and this description to match your project!
 
 "use strict";
 
-// Code goes here
+window.onload = function() {
+    //??? why option not loading
+let menu= document.getElementById("menu-select");
+let currentOption=null;
+
+//select menu:
+if (menu.value === "1"){
+    currentOption=1;
+    console.log(currentOption);
+} else if (menu.value === "2"){
+    currentOption=2;
+    console.log(currentOption);
+} else if (menu.value === "0"){
+    currentOption=0;
+    console.log(currentOption);
+}
+
+if (currentOption === 1){
+    //OPEN WINDOW GOLDEN RATIO
+    document.getElementById("goldenRatio-container").style.display="block";
+    document.getElementById("cartesian-container").style.display="none";
+} else if (currentOption === 2){
+        //OPEN WINDOW GRID
+    document.getElementById("goldenRatio-container").style.display="none";
+    document.getElementById("cartesian-container").style.display="block";
+} else {
+    console.log("nananan")
+}
+
+}
